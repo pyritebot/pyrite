9042d8f7a573a7993d9c9850780bf520dbbd09ed
Ángel González Vázquez <contact@angelnext.dev>
2023-10-04 16:01:32 +0200

    chore: Update package.json

--- .config/build.sh
+++ .config/build.sh
@@ -7,7 +7,7 @@ mkdir -p ./.build
 
 ENTRYPOINTS=$(find -type f -name '*.[tj]s' -not -path './node_modules/*')
 
-esbuild $ENTRYPOINTS \
+pnpm esbuild $ENTRYPOINTS \
 	--log-level=warning \
 	--outdir='./dist' \
 	--outbase=. \
